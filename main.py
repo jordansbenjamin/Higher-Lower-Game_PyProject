e1e2faf0b42faa5733cc797325e0ebc76bcb5821
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-17 14:45:28 +1000

    Added calculate score function, will continue tomorrow

--- main.py
+++ main.py
@@ -7,16 +7,39 @@ def comparison(a_or_b):
     '''Grabs data and returns formatted result'''
     choice = random.choice(data)
     name = choice['name']
-    # f_count = choice['follower_count']
+    f_count = choice['follower_count']
     desc = choice['description']
     country = choice['country']
     print(f"{a_or_b}: {name}, a {desc}, from {country}.")
+    return f_count
+
+def calculate_count(a, b):
+    '''Checks higher follower count'''
+    if a > b:
+        return a
+    return b
+
+def calculate_count(a, b):
+    '''Checks higher follower count'''
+    return a if a > b else b
 
 def game():
     '''Main game function that handles all the games logic'''
-    print(logo)
-    comparison("Compare A")
-    print(vs)
-    comparison("Against B")
+    while True:
+        print(logo)
+        a = comparison("Compare A")
+        print(vs)
+        b = comparison("Against B")
+
+        higher_count = calculate_count(a, b)
+        
+        user_guess = input("Who has more followers? Type 'A' or 'B': ")
+
+        if user_guess == 
+
+        
+
+
+
 
 game()
