dd7a8ae1fcad834016b16d53b3404e2db78f6a90
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-17 14:30:47 +1000

    Created main game function + added ASCII art

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import random
 from game_data import data
+from art import logo, vs
 
 # Figure out how to format data from dictionary
 def comparison(a_or_b):
     '''Grabs data and returns formatted result'''
     choice = random.choice(data)
     name = choice['name']
-    f_count = choice['follower_count']
+    # f_count = choice['follower_count']
     desc = choice['description']
     country = choice['country']
-    return f"{a_or_b}: {name}, a {desc}, from {country}."
+    print(f"{a_or_b}: {name}, a {desc}, from {country}.")
 
-print(comparison("Compare A"))
-print(comparison("Against B"))
+def game():
+    '''Main game function that handles all the games logic'''
+    print(logo)
+    comparison("Compare A")
+    print(vs)
+    comparison("Against B")
+
+game()
