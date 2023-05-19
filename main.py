b766320e45e6fa12921f4fb5675199bc6ecec66b
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-19 19:43:17 +1000

    Added venv + clear package

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
+from clear import clear
 from game_data import data
 from art import logo, vs
 
 # Figure out how to format data from dictionary
-def comparison(a_or_b):
+def comparison(random, a_or_b):
     '''Grabs data and returns formatted result'''
-    choice = random.choice(data)
+    # choice = random.choice(data)
+    choice = random
     name = choice['name']
     f_count = choice['follower_count']
     desc = choice['description']
@@ -26,34 +28,46 @@ def calculate_count(a, b):
 
 def game():
     '''Main game function that handles all the games logic and flow'''
+    score = 0
+    winning_choice = None
     while True:
-        score = 0
-        winning_choice = None
+        random.shuffle(data)
+        random_a = data.pop()
+        random_b = data.pop()
+        clear()
         print(logo)
 
         if score > 0:
-            a = winning_choice
+            random_a = winning_choice
             print(f"You're right! Current score: {score}")
 
-        a = comparison("Compare A")
+        a = comparison(random_a, "Compare A")
         print(vs)
-        b = comparison("Against B")
+        b = comparison(random_b, "Against B")
 
         higher_count = calculate_count(a, b)
+        print(a, b)
         
         guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 
         if guess == 'A':
-            check_guess(a, score, higher_count)
-            continue
+            # check_guess(a, score, higher_count)
+            guess = a
+            if guess == higher_count:
+                score += 1
+                winning_choice = a
+                continue
         elif guess == 'B':
-            check_guess(b, score, higher_count)
-            continue
+            guess = b
+            if guess == higher_count:
+                score += 1
+                winning_choice = b
+                continue
         else:
-            print(f"Sorry, that's wrong. Final score: {score}")
-            return
-
-# Figure out scoring when user guesses the correct answer, needs to print at the top under the logo
-# Else the game ends and reveal their score anyways
+            break
+        clear()
+        print(logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        return
 
 game()
