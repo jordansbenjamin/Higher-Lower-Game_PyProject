d3d4dfcf697d9256bfdaefe6ea7fc1d309f20f04
jordansbenjamin <14327@coderacademy.edu.au>
2023-05-19 19:20:21 +1000

    Continued working on logic of the game

--- main.py
+++ main.py
@@ -13,33 +13,47 @@ def comparison(a_or_b):
     print(f"{a_or_b}: {name}, a {desc}, from {country}.")
     return f_count
 
-def calculate_count(a, b):
-    '''Checks higher follower count'''
-    if a > b:
-        return a
-    return b
 
 def calculate_count(a, b):
     '''Checks higher follower count'''
     return a if a > b else b
 
+# def check_guess(a_or_b, score, higher_count):
+#     guess = a_or_b
+#     if guess == higher_count:
+#         score += 1
+#         winning_choice = a_or_b
+
 def game():
-    '''Main game function that handles all the games logic'''
+    '''Main game function that handles all the games logic and flow'''
     while True:
+        score = 0
+        winning_choice = None
         print(logo)
+
+        if score > 0:
+            a = winning_choice
+            print(f"You're right! Current score: {score}")
+
         a = comparison("Compare A")
         print(vs)
         b = comparison("Against B")
 
         higher_count = calculate_count(a, b)
         
-        user_guess = input("Who has more followers? Type 'A' or 'B': ")
+        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 
-        if user_guess == 
+        if guess == 'A':
+            check_guess(a, score, higher_count)
+            continue
+        elif guess == 'B':
+            check_guess(b, score, higher_count)
+            continue
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            return
 
-        
-
-
-
+# Figure out scoring when user guesses the correct answer, needs to print at the top under the logo
+# Else the game ends and reveal their score anyways
 
 game()
